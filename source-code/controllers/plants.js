97ff7b577867a25c8f19b252cc5caa041b0de65a
BurakEmreGundes <burak.gundes@ogr.sakarya.edu.tr>
2022-12-06 14:01:55 +0300

    added asyncHandler and errorHandler

--- source-code/controllers/plants.js
+++ source-code/controllers/plants.js
@@ -1,44 +1,66 @@
 const Plant = require("../models/Plant")
+const ErrorResponse = require("../utils/errorResponse")
+const asyncHandler = require("../middlewares/async")
 
 
 // @desc    Get all plants
 // @route   GET /api/v1/plants
 // @access  Public
-exports.getPlants = (req, res, next) =>{
-    res.status(200).json({success: true, msg: "Show all Plants"})
-}
+exports.getPlants = asyncHandler(async (req, res, next) =>{
+        const plants = await Plant.find()
+
+        res.status(200).json({
+            success: true, data: plants
+        })
+})
 
 // @desc    Get single plant
 // @route   GET /api/v1/plants/:id
 // @access  Public
-exports.getPlant = (req, res, next) =>{
-    res.status(200).json({success: true, msg: "Show one plant"})
-}
+exports.getPlant = asyncHandler(async (req, res, next) =>{
+        const plant = await Plant.findById(req.params.id)
+
+        if(!plant){
+            return next(new ErrorResponse(`Plant not found with id of ${req.params.id}`,404))
+        }
+
+        res.status(200).json({success: true, data: plant})
+})
 
 // @desc    Create new plant
 // @route   GET /api/v1/plants
 // @access  Private
-exports.createPlant = async (req, res, next) =>{
-    try {
+exports.createPlant = asyncHandler(async (req, res, next) =>{
         const plant = await  Plant.create(req.body)
         res.status(201).json({success: true, data: plant})        
-    } catch (err) {
-        res.status(400).json({success: false, err: err.message})
-    }
-
-}
+})
 
 // @desc    Update plant
 // @route   PUT /api/v1/plants/:id
 // @access  Private
-exports.updatePlant = (req, res, next) =>{
-    res.status(200).json({success: true, msg: "Update plant"})
-}
+exports.updatePlant = asyncHandler(async (req, res, next) =>{
+        const plant = await Plant.findByIdAndUpdate(req.params.id, req.body,{
+            new:true,
+            runValidators:true
+        })
+
+        if(!plant){
+            return next(new ErrorResponse(`Plant not found with id of ${req.params.id}`,404))
+        }
+
+        res.status(200).json({success: true, data: plant})
+})
 
 
 // @desc    Delete plant
 // @route   DELETE /api/v1/plants/:id
 // @access  Private
-exports.deletePlant = (req, res, next) =>{
-    res.status(200).json({success: true, msg: "Delete plant"})
-}
+exports.deletePlant = asyncHandler(async (req, res, next) =>{
+        const plant = await Plant.findByIdAndDelete(req.params.id)
+
+        if(!plant){
+            return next(new ErrorResponse(`Plant not found with id of ${req.params.id}`,404))
+        }
+
+        res.status(200).json({success: true, data: {}})
+})
